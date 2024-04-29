13ff01b124998e8829f1d209e3cbbfa934484a18
Kathy Bailey Hines <135627670+klbailey@users.noreply.github.com>
2024-04-29 09:06:52 -0700

    Chat Project

---
+++ core/chat/models.py
@@ -0,0 +1,33 @@
+# core>chat/models.py
+
+from django.contrib.auth.models import User
+from django.db import models
+
+# Chat room with online field for tracking users connect/disconnect from room
+class Room(models.Model):
+    name = models.CharField(max_length=128)
+    online = models.ManyToManyField(to=User, blank=True)
+
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+        self.save()
+
+    def leave(self, user):
+        self.online.remove(user)
+        self.save()
+
+    def __str__(self):
+        return f'{self.name} ({self.get_online_count()})'
+
+# Message sent to chat room/store messages sent in chat
+class Message(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content} [{self.timestamp}]'
